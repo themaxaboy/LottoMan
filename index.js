f4096ff1a6d6627ce37e09b86153b447c78c8d5e
themaxaboy <themaxaboy@hotmail.com>
2017-06-21 17:48:12 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ function handleEvent(event) {
     var reg = new RegExp("\\d{6}");
     if (reg.test(event.message.text)) {
         var onlyNumber = reg.exec(event.message.text);
-        var data = price.checkPrice(onlyNumber);
+        var data = price.checkPrice(onlyNumber+'');
         for (var i in data) {
             messengToUser += ' ' + data[i].text;
         }
